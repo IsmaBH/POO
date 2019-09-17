b28d0c4c4f6645b73891ceedd5dd374732467124
IsmaBH <deigo-b@hotmail.com>
2019-09-17 11:04:03 -0500

    Se agrega el sub ejercicio de agregacion y composicion, la clase banco aun no tiene sus metodos completados

---
+++ Baquito/Banco/Banco.java
@@ -0,0 +1,26 @@
+public class Banco{
+	private Cliente[] clientes;
+	private int numClientes;
+	//Constructor
+	public Banco(){
+		this.clientes = NULL;
+		this.numClientes = 0;
+	}
+	//Metodos
+	//agregaCliente: Este metodo agrega un cliente a la lista de clientes
+	public void agregaCliente(String nom, String cta){
+		//code
+	}
+	//obtenerNumClientes: ESte metodo obtiene el numero de clientes totales del banco
+	public int obtenerNumClientes(){
+		return numClientes;
+	}
+	//obtenerCliente: Este metodo obtiene un cliente especifico de la lista de clientes
+	public Cliente obtenerCliente(int indice){
+		//code
+	}
+	//imprimirClientes: Este metodo despliega en pantalla todos los clientes de la lista
+	public void imprimirCLientes(){
+		//code
+	}
+}
